4a47e594fcdb7ebb1d2714aaa868c96be762bf5d
kaspr88 <kaspr88@mail.ru>
2022-12-20 14:41:59 +0600

    create

---
+++ С#/DZ_C#/DZ1/Task 1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число");
+int Number1 = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine("Введите второе число");
+int Number2 = int.Parse(Console.ReadLine() ?? "");
+if (Number1 > Number2)
+{
+    Console.WriteLine($"{Number1}>{Number2}");
+    Console.WriteLine($"Max={Number1}");
+}
+else
+{
+    Console.WriteLine($"{Number1}<{Number2}");
+    Console.WriteLine($"Max={Number2}");
+};
