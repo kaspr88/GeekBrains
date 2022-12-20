d213162e1e20315b5b32f9f9a862c64d2e0cedef
kaspr88 <kaspr88@mail.ru>
2022-12-20 15:26:49 +0600

    Task 1

--- С#/DZ_C#/DZ2/Task 1/Program.cs
+++ С#/DZ_C#/DZ2/Task 1/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая принимает на вход трёхзначное
+ число и на выходе показывает вторую цифру этого числа.*/
+Console.WriteLine("Введите трехзначное число");
+int number = int.Parse(Console.ReadLine() ?? "");
+int a = number % 100;
+int b = a / 10;
+Console.WriteLine($"{number}->{b}");
