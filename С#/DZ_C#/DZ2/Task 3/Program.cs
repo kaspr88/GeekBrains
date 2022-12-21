77d1e7fbb02fd8fd2f4bbecbf2d5f858ad8e18a6
kaspr88 <kaspr88@mail.ru>
2022-12-21 10:25:35 +0600

    Task 3

--- С#/DZ_C#/DZ2/Task 3/Program.cs
+++ С#/DZ_C#/DZ2/Task 3/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая принимает на вход цифру, обозначающую 
+день недели, и проверяет, является ли этот день выходным.*/
+Console.Write("Введите число от 1 до 7:");
+int number = int.Parse(Console.ReadLine() ?? "");
+if (number < 7) Console.WriteLine($"{number} -> да");
+else Console.WriteLine($"{number} -> нет");
