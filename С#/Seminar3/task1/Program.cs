0a180d9fed6761c9ce62ca3b2047bb655c560e57
kaspr88 <kaspr88@mail.ru>
2022-12-23 01:03:38 +0600

    Суминар 3

---
+++ С#/Seminar3/task1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Ведите номер четверти");
+int num = int.Parse(Console.ReadLine() ?? "");
+if (num == 1)
+{
+    Console.WriteLine("x > 0, y > 0");
+}
+else if (num == 2)
+{
+    Console.WriteLine("x < 0, y > 0");
+}
+else if (num == 3)
+{
+    Console.WriteLine("x < 0, y < 0");
+}
+else if (num == 4)
+{
+    Console.WriteLine("x > 0, y < 0");
+}
+else
+{
+    Console.WriteLine("Вы ввели номер не существующей четверти");
+}
