b308aeb4153028da9d9dd93581ab5381e61d458d
kaspr88 <kaspr88@mail.ru>
2022-12-23 01:02:16 +0600

    Семинар 3

---
+++ С#/Seminar3/task2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Ведите координаты точки A");
+int x = int.Parse(Console.ReadLine() ?? "");
+int y = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine("Ведите координаты точки B");
+int x1 = int.Parse(Console.ReadLine() ?? "");
+int y1 = int.Parse(Console.ReadLine() ?? "");
+
+
+
+int a = Math.Sqrt(Math.Pow((x1 - x), 2) + Math.Pow((y1 - y), 2));
+
+Console.WriteLine(a);
