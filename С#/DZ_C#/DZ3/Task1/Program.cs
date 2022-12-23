a089bc9ee0e7dc8f6b752fc0a701d27ac6d4ef14
kaspr88 <kaspr88@mail.ru>
2022-12-23 21:43:23 +0600

    Task1

--- С#/DZ_C#/DZ3/Task1/Program.cs
+++ С#/DZ_C#/DZ3/Task1/Program.cs
@@ -1,15 +1,31 @@
-﻿Console.Clear();
-Console.WriteLine("Hello, World!");
-void GetRectangleData(in int width, in int height, out int rectArea, out int rectPerimetr)
+﻿
+/*Напишите программу, которая принимает на вход пятизначное число 
+и проверяет, является ли оно палиндромом.*/
+static void inputNumber(ref int num)
 {
-    //width = 25; // нельзя изменить, так как width - входной параметр
-    rectArea = width * height;
-    rectPerimetr = (width + height) * 2;
+    try
+    {
+        Console.Write("Введите пятизначное число: ");
+        num = int.Parse(Console.ReadLine() ?? "");
+    }
+    catch (Exception ex)
+    {
+        Console.Write($"Ошибка ввода данных:{ex.Message}");
+        return;
+    }
 }
 
-int w = 20;
-int h = 20;
-GetRectangleData(w, h, out var area, out var perimetr);
+static void printNumber(int num)
+{
+    int left1 = num / 10000;
+    int left2 = (num / 1000) % 10;
+    int right1 = num % 10;
+    int right2 = (num % 100) / 10;
 
-Console.WriteLine($"Площадь прямоугольника: {area}");       // 200
-Console.WriteLine($"Периметр прямоугольника: {perimetr}");
+    if (left1 == right1 || left2 == right2) Console.Write($"{num} -> да");
+    else Console.Write($"{num} -> нет");
+}
+
+int x = 0;
+inputNumber(ref x);
+printNumber(x);
