04a06cf588337174bbe3f57ec2a4099d308f8cc8
kaspr88 <kaspr88@mail.ru>
2022-12-23 16:28:06 +0600

    task1-3

---
+++ С#/DZ_C#/DZ3/Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Hello, World!");
+void GetRectangleData(in int width, in int height, out int rectArea, out int rectPerimetr)
+{
+    //width = 25; // нельзя изменить, так как width - входной параметр
+    rectArea = width * height;
+    rectPerimetr = (width + height) * 2;
+}
+
+int w = 20;
+int h = 20;
+GetRectangleData(w, h, out var area, out var perimetr);
+
+Console.WriteLine($"Площадь прямоугольника: {area}");       // 200
+Console.WriteLine($"Периметр прямоугольника: {perimetr}");
