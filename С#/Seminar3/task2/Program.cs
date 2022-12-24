d50f32c0c22aa43a5e17157d3e055b1de78804d7
kaspr88 <kaspr88@mail.ru>
2022-12-24 22:34:50 +0600

    Task2

--- С#/Seminar3/task2/Program.cs
+++ С#/Seminar3/task2/Program.cs
@@ -1,12 +1,12 @@
 ﻿Console.WriteLine("Ведите координаты точки A");
-int x = int.Parse(Console.ReadLine() ?? "");
-int y = int.Parse(Console.ReadLine() ?? "");
+int ax = int.Parse(Console.ReadLine() ?? "");
+int ay = int.Parse(Console.ReadLine() ?? "");
 Console.WriteLine("Ведите координаты точки B");
-int x1 = int.Parse(Console.ReadLine() ?? "");
-int y1 = int.Parse(Console.ReadLine() ?? "");
+int bx = int.Parse(Console.ReadLine() ?? "");
+int by = int.Parse(Console.ReadLine() ?? "");
 
 
 
-int a = Math.Sqrt(Math.Pow((x1 - x), 2) + Math.Pow((y1 - y), 2));
+double result = Math.Round(Math.Sqrt(Math.Pow((ax - bx), 2) + Math.Pow((ay - by), 2)), 2);
 
-Console.WriteLine(a);
+Console.WriteLine(result);
