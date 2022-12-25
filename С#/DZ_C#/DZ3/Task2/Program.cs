674aff982c66b305391498eb42e68a6ea0ebcea9
kaspr88 <kaspr88@mail.ru>
2022-12-25 21:40:11 +0600

    task2

--- С#/DZ_C#/DZ3/Task2/Program.cs
+++ С#/DZ_C#/DZ3/Task2/Program.cs
@@ -8,17 +8,17 @@ static void inputDotCoor(ref int ax, ref int ay, ref int az, ref int bx, ref int
 {
     try
     {
-        Console.WriteLine("Введите ax");
+        Console.Write("Введите ax:");
         ax = int.Parse(Console.ReadLine() ?? "");
-        Console.WriteLine("Введите ay");
+        Console.Write("Введите ay:");
         ay = int.Parse(Console.ReadLine() ?? "");
-        Console.WriteLine("Введите az");
+        Console.Write("Введите az:");
         az = int.Parse(Console.ReadLine() ?? "");
-        Console.WriteLine("Введите bx");
+        Console.Write("Введите bx:");
         bx = int.Parse(Console.ReadLine() ?? "");
-        Console.WriteLine("Введите by");
+        Console.Write("Введите by:");
         by = int.Parse(Console.ReadLine() ?? "");
-        Console.WriteLine("Введите bz");
+        Console.Write("Введите bz:");
         bz = int.Parse(Console.ReadLine() ?? "");
     }
     catch (Exception ex)
@@ -27,15 +27,14 @@ static void inputDotCoor(ref int ax, ref int ay, ref int az, ref int bx, ref int
         return;
 
     }
-
-    static void lengthSegment(int ax, int ay, int az, int bx, int by, int bz)
-    {
-        double result = Math.Round(Math.Sqrt(Math.Pow((ax - bx), 2) + Math.Pow((ay - by), 2) + Math.Pow((az - bz), 2)), 2);
-        Console.WriteLine(result);
-
-    }
     lengthSegment(ax, ay, az, bx, by, bz);
 }
+static void lengthSegment(int ax, int ay, int az, int bx, int by, int bz)
+{
+    Console.Clear();
+    double result = Math.Round(Math.Sqrt(Math.Pow((ax - bx), 2) + Math.Pow((ay - by), 2) + Math.Pow((az - bz), 2)), 2);
+    Console.WriteLine($"A ({ax}, {ay}, {az}); B ({bx}, {by}, {bz}) -> {result}");
 
+}
 
 
