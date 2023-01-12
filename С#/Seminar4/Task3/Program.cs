853c69fa34578799470b4433e43984b6e3bc3b3b
kaspr88 <kaspr88@mail.ru>
2023-01-13 00:50:58 +0600

    Seminar3

---
+++ С#/Seminar4/Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿GetArr();
+
+int GetArr()
+{
+    int numbers = new Random().Next(0, 2);
+    for (int i = 0; i < 8; i++)
+    {
+        int numbers = new Random().Next(0, 2);
+        Console.WriteLine($"{numbers}");
+        if (inde < 7) Console.Write(",");
+
+    }
+
+
+}
