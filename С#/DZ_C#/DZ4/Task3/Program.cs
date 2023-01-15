7744958bdd896cd36b007563867f2adeed64d625
kaspr88 <kaspr88@mail.ru>
2023-01-15 20:47:00 +0600

    DZ4T3

---
+++ С#/DZ_C#/DZ4/Task3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+int num = inputArr("Введите элемент массива:");
+Console.Write($"{num}");
+//Console.Write(getNumber(num));
+
+int inputArr(string mesage)
+{
+    int namber = 0;
+    while (namber <= 8)
+    {
+        Console.Write(mesage + $" {namber}");
+        int[] arr = new int[int.Parse(Console.ReadLine() ?? "")];
+        namber++;
+    }
+    return namber;
+}
+
+
+/*
+int getNumber(int num)
+{
+    int n = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        n = arr[i];
+    }
+    return n;
+}*/
