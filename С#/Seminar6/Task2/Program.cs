8255d453e4d95246a2099075d994724794afe8d9
kaspr88 <kaspr88@mail.ru>
2023-01-20 00:29:41 +0600

    Sem6

---
+++ С#/Seminar6/Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+
+static void inputNumber(int num)
+{
+    try
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write("Введите пятизначное число: ");
+            int num = int.Parse(Console.ReadLine() ?? "");
+            arr[i] = num;
+        }
+
+    }
+    catch (Exception ex)
+    {
+        Console.Write($"Ошибка ввода данных:{ex.Message}");
+        return;
+    }
+}
