3ff301da7f9b4893ae975bf95c87cd90a85ce7a9
kaspr88 <kaspr88@mail.ru>
2023-01-19 16:37:27 +0600

    DZ

---
+++ С#/DZ_C#/DZ5/Task3/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+Console.Clear();
+int[] array = RandomArray(10, 0, 100);
+int defValue = sumValue(array);
+Console.WriteLine($"[{String.Join(" ", array)}] --> {defValue} ");
+//<-----Разниц между максимальным и минимальным элементов массива----->//
+int sumValue(int[] arr)
+{
+    int max = arr[0];
+    int min = arr[0];
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        if (max < arr[i])
+        {
+            max = array[i];
+        }
+        if (min > arr[i])
+        {
+            min = array[i];
+        }
+    }
+    int def = max - min;
+    return def;
+}
+//<--------------Массив заполненный случайными числами---------------->//
+int[] RandomArray(int size, int minValue, int maxValue)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(minValue, maxValue);
+    }
+    return arr;
+}
