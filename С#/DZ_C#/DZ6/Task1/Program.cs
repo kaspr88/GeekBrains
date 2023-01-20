a6d3114e9b820c44aa5f2ab9db90fe40a04f1b91
kaspr88 <kaspr88@mail.ru>
2023-01-20 16:47:13 +0600

    DZ6

---
+++ С#/DZ_C#/DZ6/Task1/Program.cs
@@ -0,0 +1,35 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+int[] Arr = inputArr("Введите число:", "Ошибка ввода");
+int res = result(Arr);
+Console.WriteLine($"{String.Join(",", Arr)} --> {res}");
+int[] inputArr(string mesage, string error)
+{
+    int[] arr = new int[8];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{mesage} {i} из {arr.Length}:");
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int n);
+        arr[i] = n;
+        if (!isCorrect)
+        {
+            Console.WriteLine(error);
+            break;
+        };
+    }
+    return arr;
+}
+
+int result(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            result++;
+        }
+    }
+
+    return result;
+
+}
--- С#/Seminar6/Task2/Program.cs
+++ С#/Seminar6/Task2/Program.cs
@@ -1,21 +1,37 @@
 ﻿//Напишите программу, которая будет преобразовывать десятичное число в двоичное.
-
-
-static void inputNumber(int num)
+Console.Write("Введите число: ");
+int n = inputNumber();
+int longArr = longArray(n);
+int[] arr = ConvertDoubleSystem(n, longArr);
+Console.WriteLine($"{n} -- > {String.Join("", arr)}");
+int[] ConvertDoubleSystem(int num, int longArray)
 {
-    try
+    int[] array = new int[longArray];
+    int a = 0;
+    for (int i = 0; i < array.Length; i++)
     {
-        for (int i = 0; i < arr.Length; i++)
-        {
-            Console.Write("Введите пятизначное число: ");
-            int num = int.Parse(Console.ReadLine() ?? "");
-            arr[i] = num;
-        }
+        a = num % 2;
+        num = num / 2;
+        array[i] = a;
+    }
+    return array;
+}
+int inputNumber()
+{
+    int num = int.Parse(Console.ReadLine() ?? "");
+    return num;
+}
+int longArray(int num)
+{
+    int a = 0;
+    int b = 0;
+    for (int i = 0; ; i++)
+    {
+        a = num % 2;
+        num = num / 2;
+        b = i;
+        if (num == 0) break;
 
     }
-    catch (Exception ex)
-    {
-        Console.Write($"Ошибка ввода данных:{ex.Message}");
-        return;
-    }
+    return b + 1;
 }
