d614b585be5f05c637cee7857045ee24f9cb152c
kaspr88 <kaspr88@mail.ru>
2023-01-23 00:11:44 +0600

    Tetris1

--- С#/Seminar7/Tetris/Program.cs
+++ С#/Seminar7/Tetris/Program.cs
@@ -1,18 +1,68 @@
 ﻿Console.Clear();
 string[] array = new string[20];
 string[] array2 = new string[10];
-for (int i = 0; i < array.Length; i++)
+positionDefault(ref array, ref array2);
+string directionInfo = enterDirection();
+while (true)
 {
-    for (int j = 0; j < array2.Length; j++)
-    {
 
-        array[j] = " . ";
-        if ((j == 4) && (i == 0)) array[j] = " + ";
-        if ((j == 5) && (i == 0)) array[j] = " + ";
-        if ((j == 4) && (i == 1)) array[j] = " + ";
-        if ((j == 5) && (i == 1)) array[j] = " + ";
-        Console.Write(array[j]);
+    if (directionInfo == "d")
+    {
+        moveToLeft(directionInfo, ref array, ref array2);
+    }
+    enterDirection();
+}
+
+
+//////////////////////////////////////
+string enterDirection()
+{
+    string direction = "0";
+    try
+    {
+        Console.WriteLine("Введите направление");
+        direction = Console.ReadLine();
 
     }
-    Console.WriteLine("");
+    catch (Exception ex)
+    {
+        Console.WriteLine("Введите нужное направлени", ex);
+    }
+    return direction;
+}
+////////////////////////////////
+void positionDefault(ref string[] array, ref string[] array2)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        for (int j = 0; j < array2.Length; j++)
+        {
+            array[j] = " . ";
+            if ((j == 4) && (i == 0)) array[j] = " + ";
+            if ((j == 5) && (i == 0)) array[j] = " + ";
+            if ((j == 4) && (i == 1)) array[j] = " + ";
+            if ((j == 5) && (i == 1)) array[j] = " + ";
+            Console.Write(array[j]);
+        }
+        Console.WriteLine("");
+    }
+
+
+}
+/////////////////////////////////
+void moveToLeft(string value, ref string[] array, ref string[] array2)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        for (int j = 0; j < array2.Length; j++)
+        {
+            array[j] = " . ";
+            if ((j == j + 1) && (i == 0)) array[j] = " + ";
+            if ((j == j + 1) && (i == 0)) array[j] = " + ";
+            if ((j == j + 1) && (i == 1)) array[j] = " + ";
+            if ((j == j + 1) && (i == 1)) array[j] = " + ";
+            Console.Write(array[j]);
+        }
+        Console.WriteLine("");
+    }
 }
