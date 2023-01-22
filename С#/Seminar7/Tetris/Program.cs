ced713321207ebb8cb6579514563b77576a9a4cb
kaspr88 <kaspr88@mail.ru>
2023-01-22 22:26:38 +0600

    Tetris

---
+++ С#/Seminar7/Tetris/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+string[] array = new string[20];
+string[] array2 = new string[10];
+for (int i = 0; i < array.Length; i++)
+{
+    for (int j = 0; j < array2.Length; j++)
+    {
+
+        array[j] = " . ";
+        if ((j == 4) && (i == 0)) array[j] = " + ";
+        if ((j == 5) && (i == 0)) array[j] = " + ";
+        if ((j == 4) && (i == 1)) array[j] = " + ";
+        if ((j == 5) && (i == 1)) array[j] = " + ";
+        Console.Write(array[j]);
+
+    }
+    Console.WriteLine("");
+}
