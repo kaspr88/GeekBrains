cb12a4ad1aa03fdf2b0621746f93a335da4b00c9
kaspr88 <kaspr88@mail.ru>
2023-01-23 20:31:45 +0600

    tetra2301

--- С#/Seminar7/Tetris/Program.cs
+++ С#/Seminar7/Tetris/Program.cs
@@ -1,27 +1,35 @@
 ﻿Console.Clear();
 string[] array = new string[20];
 string[] array2 = new string[10];
-positionDefault(ref array, ref array2);
-string directionInfo = enterDirection();
+int a = 4;
+int b = 5;
+int c = 4;
+int d = 5;
+int e = 0;
+int f = 0;
+int g = 1;
+int h = 1;
+
 while (true)
 {
-
-    if (directionInfo == "d")
-    {
-        moveToLeft(directionInfo, ref array, ref array2);
-    }
-    enterDirection();
+    positionDefault(ref a, ref b, ref c, ref d);
+    string directionInfo = enterDirection();
+    moveToLeftToRight(directionInfo, ref a, ref b, ref c, ref d);
+    moveToBottom(directionInfo, ref e, ref f, ref g, ref h);
+    Console.Clear();
 }
-
-
 //////////////////////////////////////
+// создаем новый поток
+//Thread myThread = new Thread(enterDirection);
+// запускаем поток myThread
+//myThread.Start();
 string enterDirection()
 {
     string direction = "0";
     try
     {
         Console.WriteLine("Введите направление");
-        direction = Console.ReadLine();
+        direction = Console.ReadLine() ?? "";
 
     }
     catch (Exception ex)
@@ -31,17 +39,17 @@ string enterDirection()
     return direction;
 }
 ////////////////////////////////
-void positionDefault(ref string[] array, ref string[] array2)
+void positionDefault(ref int a, ref int b, ref int c, ref int d)
 {
     for (int i = 0; i < array.Length; i++)
     {
         for (int j = 0; j < array2.Length; j++)
         {
             array[j] = " . ";
-            if ((j == 4) && (i == 0)) array[j] = " + ";
-            if ((j == 5) && (i == 0)) array[j] = " + ";
-            if ((j == 4) && (i == 1)) array[j] = " + ";
-            if ((j == 5) && (i == 1)) array[j] = " + ";
+            if ((j == a) && (i == e)) array[j] = " + ";
+            if ((j == b) && (i == f)) array[j] = " + ";
+            if ((j == c) && (i == g)) array[j] = " + ";
+            if ((j == d) && (i == h)) array[j] = " + ";
             Console.Write(array[j]);
         }
         Console.WriteLine("");
@@ -50,19 +58,30 @@ void positionDefault(ref string[] array, ref string[] array2)
 
 }
 /////////////////////////////////
-void moveToLeft(string value, ref string[] array, ref string[] array2)
+void moveToLeftToRight(string value, ref int a, ref int b, ref int c, ref int d)
 {
-    for (int i = 0; i < array.Length; i++)
+
+
+    if (value == "d")
     {
-        for (int j = 0; j < array2.Length; j++)
-        {
-            array[j] = " . ";
-            if ((j == j + 1) && (i == 0)) array[j] = " + ";
-            if ((j == j + 1) && (i == 0)) array[j] = " + ";
-            if ((j == j + 1) && (i == 1)) array[j] = " + ";
-            if ((j == j + 1) && (i == 1)) array[j] = " + ";
-            Console.Write(array[j]);
-        }
-        Console.WriteLine("");
+        a = a + 1;
+        b = b + 1;
+        c = c + 1;
+        d = d + 1;
     }
+    if (value == "a")
+    {
+        a = a - 1;
+        b = b - 1;
+        c = c - 1;
+        d = d - 1;
+    }
+}
+/////////////////////
+void moveToBottom(string value, ref int e, ref int f, ref int g, ref int h)
+{
+    e = e + 1;
+    f = f + 1;
+    g = g + 1;
+    h = h + 1;
 }
