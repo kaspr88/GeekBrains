fa2ae36243a2ff7901ab79eccb6c7d690222d634
kaspr88 <kaspr88@mail.ru>
2023-01-30 21:17:24 +0600

    DZ8

--- С#/DZ_C#/DZ8/Task5/Program.cs
+++ С#/DZ_C#/DZ8/Task5/Program.cs
@@ -1,38 +1,65 @@
 ﻿// Напишите программу, которая заполнит спирально массив 4 на 4
 
-int[,] array = fillArray();
-printArray(array);
-
-
-int[,] fillArray()
+int size = inputSizeArray();
+int[,] array = new int[size, size];
+FillArray(array, size);
+PrintArray(array);
+/////////////////Ввод размерности массива///////////////////////////
+int inputSizeArray()
 {
-    int[,] arr = new int[4, 4];
     int a = 0;
-    for (int i = 0; i < arr.GetLength(0); i++)
+    try
     {
+        Console.Write("Введите размер массива: ");
+        a = int.Parse(Console.ReadLine() ?? "");
 
-        for (int j = 0; j < arr.GetLength(1); j++)
-        {
-            for (int k = 0; k < arr.GetLength(0); k++)
-            {
-                //a = a + arr[i, k];
-                arr[i, k] = a + i;
-            }
-
-        }
     }
-    return arr;
+    catch (Exception ex)
+    {
+        Console.Write("Не верный формат", ex);
+    }
+    return a;
 }
-
-void printArray(int[,] arr)
+///////////////Функция заполнения массива//////////////////////
+void FillArray(int[,] array, int size)
 {
-    for (int i = 0; i < arr.GetLength(0); i++)
+    int i = 0, j = 0;
+    int value = 1;
+    for (int e = 0; e < size * size; e++)
     {
-        for (int j = 0; j < arr.GetLength(1); j++)
+        int k = 0;
+        do { array[i, j++] = value++; }
+        while (++k < size - 1);
+        for (k = 0; k < size - 1; k++)
         {
-            Console.Write($" {arr[i, j]}");
+            array[i++, j] = value++;
         }
-        Console.WriteLine("");
+        for (k = 0; k < size - 1; k++)
+        {
+            array[i, j--] = value++;
+        }
+        for (k = 0; k < size - 1; k++)
+        {
+            array[i--, j] = value++;
+        }
+        ++i; ++j;
+        size = size < 2 ? 0 : size - 2;
     }
-    Console.WriteLine("");
+}
+//////////////////  Вывод массива ////////////////
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] < 10)
+            {
+                Console.Write("0" + array[i, j]);
+                Console.Write(" ");
+            }
+            else Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
 }
