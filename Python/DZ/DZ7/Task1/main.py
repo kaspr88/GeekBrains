369f0c250dd7fcf2ab948442a0f771f45a3de5d5
kaspr88 <kaspr88@mail.ru>
2023-04-06 09:45:31 +0600

    save

--- Python/DZ/DZ7/Task1/main.py
+++ Python/DZ/DZ7/Task1/main.py
@@ -7,24 +7,21 @@
 **Ввод:** пара-ра-рам рам-пам-папам па-ра-па-да    
     **Вывод:** Парам пам-пам '''
 
-Vini_Puh = 'пара-ри-рам рам-пум-папам пу-ра-пи-да '
-
-print(len(Vini_Puh))
+Vini_Puh = 'пара-ри-рам рам-пум-папам пу-ра-пи-да'
 
 
 def parampam(pam):
     b = 0
     c = []
-    d = 0
     for i in pam:
-        d = d+1
-        print(d)
         if i in "аеёиоуыэюя":
             b += 1
         elif i == " ":
+            print(b)
             # b += 1
             c.append(b)
             b = 0
+        print(b)
     e = 0
     for i in range(len(c)-1):
         e = c[i] - c[i+1]
