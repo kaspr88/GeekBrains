889586ac9f21bcabd01d9c6d01990c3de41efdee
kaspr88 <kaspr88@mail.ru>
2023-04-11 11:31:36 +0600

    dz8

--- Python/DZ/DZ8/main.py
+++ Python/DZ/DZ8/main.py
@@ -10,59 +10,69 @@
 
 def data_input():
     users = {}
-    users['surname'] = (input("Введите Фамилию:"))
-    users['phone'] = (input("Введите телефона:"))
-    users['name'] = (input("Введите Имя:"))
-    users['patronymic'] = (input("Введите Отчество:"))
+    users['surname'] = (input("Введите Фамилию: "))
+    users['phone'] = (input("Введите телефона: "))
+    users['name'] = (input("Введите Имя: "))
+    users['patronymic'] = (input("Введите Отчество: "))
     return users
 
 
 def data_save(data_input):
     file = open("Python/DZ/DZ8/file1.txt", "a", encoding="utf-8")
+    file.write("\n{")
+    num = 0
     for key in data_input:
-        file.write(f"{key} {data_input[key]}\n")
+        file.write(f'"{key}": "{data_input[key]}"')
+        num += 1
+        if num != len(data_input):
+            file.write(", ")
+        else:
+            file.write("}")
     file.close()
-
-
-# data_save(data_input())
 
 
 def data_read():
     file = open("Python/DZ/DZ8/file1.txt", "r", encoding="utf-8")
-    onstring = file.read().split("\n")[:-1]
+    onstring = file.read().split("\n")
     arr = []
-    array = {}
     for item in onstring:
-        key = item.split(" ")[0]
-        value = item.split(" ")[1]
-        array[key] = value
-    arr.append(array)
-
-    print(f'{arr}\n')
+        arr.append(dict(eval(item)))
     file.close()
-
     return arr
 
 
-print(data_read())
-
-
 def data_search(data_read):
+    user = []
     input_search = input(
         "Введите Фамилию искомого абонента: ")
-    for key in data_read:
-        if input_search == data_read["surname"]:
-            print(
-                f"{data_read['phone']}, {data_read['name']}, {data_read['patronymic']}, ")
-            break
-        else:
-            return False
+    for dict in data_read:
+        for i in dict:
+            if input_search == dict[i]:
+                user.append(dict['phone'])
+                user.append(dict['name'])
+                user.append(dict['patronymic'])
+    return user
+
+def data_output(action):
+    if action == 1:
+        for i in data_search(data_read()):
+            print(f'{i}')
 
 
-def data_output(data_search, data_read):
-    file = open("Python/DZ/DZ8/file1.txt", "r", encoding="utf-8")
-    file.read()
-    file.close()
-
-
-data_output(data_search(data_read()), data_read())
+while True:
+    print("1- добавить пользователя, 2 - найти пользователя, 3 - вывести весь список, 4 - выход из программы")
+    actions = int(input("Выберете действие: "))
+    if actions == 1:
+        data_save(data_input())
+    elif actions == 2:
+        for i in data_search(data_read()):
+            print(f'{i}')
+    elif actions == 3:
+        a = data_read()
+        print(a[0])
+        for i in a:
+            for j in i:
+                print(i[j],end= "\n")
+    elif actions == 4:
+        print("Вы вышли из программы")
+        break
